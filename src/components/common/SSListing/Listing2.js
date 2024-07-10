6377d2f98dd771f5b8d37eeb154df6266ed99bdc
Bappaditya Bera <bappaditya.bera@tier5.in>
2024-07-10 15:37:07 +0530

    FRMAP-2388 New list sorting and ui update (#849)

--- src/components/common/SSListing/Listing2.js
+++ src/components/common/SSListing/Listing2.js
@@ -9,6 +9,7 @@ import { useSelector } from "react-redux";
 import helper from "../../../helpers/helper";
 import apiClient from "../../../services";
 import "./style/listingStyles.scss";
+import { MuiListStyleProps } from "./style/Style";
 
 export default function Listing2(props) {
   //mock data - strongly typed if you are using TypeScript (optional, but recommended)
@@ -37,7 +38,7 @@ export default function Listing2(props) {
     pageIndex: 0,
     pageSize: 15, //customize the default page size
   });
-
+  const listMuiProps = MuiListStyleProps(theme);
   useEffect(() => {
     console.log("rowSelection", rowSelection);
   }, [rowSelection]);
@@ -114,16 +115,11 @@ export default function Listing2(props) {
   //   console.log('selectAcross :::',  selectAcross);
   // }, [selectAcross])
 
-  // useEffect(() => {
-  //   console.log("columnFilters--- :::>>>", columnFilters);
-  //   console.log("columnFilterFns--- :::>>>", columnFilterFns);
-  // }, [columnFilters, columnFilterFns]);
-  const fetchData = async (
-    paginationData,
-    globalFilter,
-    filteronColumn,
-    filteronColumnFns
-  ) => {
+    // useEffect(() => {
+    //   console.log("columnFilters--- :::>>>", columnFilters);
+    //   console.log("columnFilterFns--- :::>>>", columnFilterFns);
+    // }, [columnFilters, columnFilterFns]);
+    const fetchData = async (paginationData, globalFilter, filteronColumn , filteronColumnFns, sortingState ) => {
     //  console.log("sorting||||--- :::>>>", sorting);
     //setData(props?.friendsData.slice( pagination.pageIndex*pagination.pageSize,pagination.pageSize*(pagination.pageIndex+1)));
     if (!data.length) {
@@ -150,13 +146,14 @@ export default function Listing2(props) {
         queryParam["filter"] = 1;
       }
 
-      console.log("sorting---outi :::>>>", sorting);
-      if (sorting.length > 0) {
-        console.log("sorting--- :::>>>", sorting);
-        queryParam["sort_by"] = sorting[0].id;
-        queryParam["sort_order"] = sorting[0].desc ? "desc" : "asc";
-      }
-      //console.log("queryParam____>>", queryParam);
+        //console.log("sorting---outi :::>>>", sortingState);
+        if(sortingState.length>0){
+          //console.log("sorting--- :::>>>", sortingState);
+          queryParam["sort_by"] = sortingState[0].id;
+          queryParam["sort_order"] = sortingState[0].desc? "desc" : "asc";
+
+        }
+        //console.log("queryParam____>>", queryParam);
 
       let response = await apiClient(
         "get",
@@ -182,367 +179,83 @@ export default function Listing2(props) {
   };
   const debouncedFetchData = useCallback(helper.debounce(fetchData, 1000), []);
 
-  useEffect(() => {
-    if (isInitialRender.current) {
-      isInitialRender.current = false; // Set to false after the first render
-    } else {
-      debouncedFetchData(
-        pagination,
-        textFilter,
-        columnFilters,
-        columnFilterFns
-      );
-    }
-  }, [textFilter, columnFilters, columnFilterFns, sorting]);
-  useEffect(() => {
-    //console.log("pagination", pagination);
-    fetchData(pagination, textFilter, columnFilters, columnFilterFns);
-  }, [pagination.pageIndex, pagination.pageSize]);
-
-  const columns = useMemo(props.listColDef, [props.listColDef]);
-
-  //light or dark white
-  //light or dark white
-  const baseBackgroundColor =
-    theme.palette.mode === "dark" ? "rgb(40, 44, 52)" : "#000000";
-
-  //pass table options to useMaterialReactTable
-  const table = useMaterialReactTable({
-    columns,
-    data,
-    rowCount,
-    getRowId: (originalRow) => originalRow.friendFbId,
-    enableRowSelection: true,
-    enableSelectAll: true,
-    selectAllMode: "page",
-    enableTopToolbar: false,
-    enableBottomToolbar: true,
-    enableColumnResizing: true,
-    enableStickyHeader: true,
-    enableStickyFooter: true,
-    enableColumnActions: false,
-    columnFilterDisplayMode: "popover",
-    enableColumnFilterModes: true,
-    manualFiltering: true,
-    enableSorting: true,
-    // manualSorting: true,
-    //enableMultiSort:true,
-    //pagination setting start
-    enablePagination: true,
-    manualPagination: true,
-    paginationDisplayMode: "pages",
-    // muiToolbarAlertBannerProps: isError
-    //   ? {
-    //     color: 'error',
-    //     children: 'Error loading data',
-    //   }
-    //   : undefined,
-    onColumnFiltersChange: setColumnFilters,
-    // onGlobalFilterChange: setGlobalFilter,
-    onPaginationChange: (pagination) => {
-      setPagination(pagination);
-    },
-    onRowSelectionChange: (selectedRows) => {
-      console.log(":::::", selectedRows());
-      setRowSelection(selectedRows);
-    },
-    //onSortingChange: setSorting,
-    onColumnFilterFnsChange: setColumnFilterFns,
-    state: {
-      rowSelection,
+    useEffect(() => {
+      if (isInitialRender.current) {
+        isInitialRender.current = false; // Set to false after the first render
+      } else {
+      debouncedFetchData(pagination,textFilter,columnFilters,columnFilterFns,sorting);
+      }
+    }, [
+      textFilter,
       columnFilters,
       columnFilterFns,
-      //isLoading,
-      pagination,
-      // showAlertBanner: isError,
-      // showProgressBars: isRefetching,
-      sorting,
-    },
-    onSortingChange: setSorting,
+    ]);
+    useEffect(() => {
+      //console.log("pagination", pagination);
+      fetchData(pagination,textFilter,columnFilters,columnFilterFns,sorting);
+    }, [pagination.pageIndex, pagination.pageSize,sorting]);
 
-    //pagination setting end
-    //styling props
-    muiTableContainerProps: { className: "mui-table-container" },
-    muiTablePaperProps: {
-      elevation: 0,
-      sx: {
-        borderRadius: "5px",
-      },
-    },
+  const columns = useMemo(props.listColDef, [props.listColDef]);
+  
 
-    // muiSelectCheckboxProps:(row, table) => ({
-    //     checked: (selectedRowIds[row?.row?.original?._id] || selectAcross?.selected) && !selectAcross?.unSelected?.includes(row?.row?.original?._id),
-    //     // color: 'primary',
-    //     onChange: (e) => {
-    //         // console.log(table.getSelectedRowModel().rows);
-    //         handleRowClick(e, row.row.original._id)
-    //     },
-    //     inputProps: {
-    //         'aria-label': `select row ${row.row.original.name}`,
-    //     },
-    //     // style: {
-    //     //   color: 'green',
-    //     // },
-    //     // size: 'small',
-    // }),
+    //pass table options to useMaterialReactTable
+    const table = useMaterialReactTable({
+        columns,
+        data,
+        rowCount,
+        getRowId: (originalRow) => originalRow.friendFbId,
+        enableRowSelection: true,
+        enableSelectAll: true,
+        selectAllMode: "page",
+        enableTopToolbar: false,
+        enableBottomToolbar: true,
+        enableColumnResizing: true,
+        enableStickyHeader: true,
+        enableStickyFooter: true,
+        enableColumnActions: false,
+        columnFilterDisplayMode: 'popover',
+        enableColumnFilterModes: true,
+        manualFiltering: true,
+        //enableSorting: true,
+        manualSorting: true,
+        // enableSortingRemoval: false,
+        // enableMultiSort:false,
+        //pagination setting start
+       // enablePagination: true,
+        manualPagination: true,
+        paginationDisplayMode: "pages",
+        // muiToolbarAlertBannerProps: isError
+        //   ? {
+        //     color: 'error',
+        //     children: 'Error loading data',
+        //   }
+        //   : undefined,
+        onColumnFiltersChange: setColumnFilters,
+        // onGlobalFilterChange: setGlobalFilter,
+        onPaginationChange: (pagination) => {
+            setPagination(pagination);
+        },
+        onRowSelectionChange: (selectedRows) => {
+          console.log(':::::', selectedRows());
+          setRowSelection(selectedRows)
+        },
+        //onSortingChange: setSorting,
+        onColumnFilterFnsChange: setColumnFilterFns,
+        state: {
+            rowSelection,
+            columnFilters,
+            columnFilterFns,
+            //isLoading,
+            pagination,
+            // showAlertBanner: isError,
+            // showProgressBars: isRefetching,
+            sorting,
+        },
+        onSortingChange: setSorting,
+        ...listMuiProps
+        //pagination setting end   
+    })
 
-    muiTableHeadProps: {
-      // Customizing table head cells
-      sx: {
-        "& .MuiTableCell-root": {
-          borderBottom: "none",
-        },
-        "& .MuiTableSortLabel-root": {
-          color: "#ffffff", // Change color of sort label text
-        },
-      },
-    },
-
-    muiTableHeadCellProps: {
-      //simple styling with the `sx` prop, works just like a style prop in this example
-      sx: {
-        fontWeight: "normal",
-        fontSize: "14px",
-        color: "#ffffff",
-        backgroundColor: "#000000",
-        height: "50px !important",
-
-        // Table Heading's Icons Color..
-        "& .MuiTableSortLabel-icon": {
-          color: "#ffffff !important", // Sort icon color
-        },
-
-        // Table Heading's Icons Color..
-        // Checkbox Icon on Header..
-        "& .MuiSvgIcon-root": {
-          color: "#B3B3BF",
-          height: "25px",
-          width: "25px",
-          borderRadius: "5px",
-        },
-        // Checked Situation on header checkbox
-        "& .Mui-checked .MuiSvgIcon-root": {
-          color: "#1976d2",
-          backgroundColor: "none",
-        },
-        // Interminate Situation on header checkbox
-        "& .MuiCheckbox-indeterminate .MuiSvgIcon-root": {
-          color: "#1976d2",
-          backgroundColor: "none",
-        },
-
-        "& .MuiCheckbox-root": {
-          color: "#0094FF",
-          backgroundColor: "none",
-        },
-
-        // Other Icons on Header..
-        "& .MuiSvgIcon-root:first-child": {
-          // marginRight: '10px',
-          // marginLeft: '10px',
-          color: "#ffffff",
-        },
-        "& span": {
-          color: "#ffffff",
-        },
-      },
-    },
-
-    // Body Styling
-    muiTableBodyRowProps: ({ row }) => ({
-      //conditionally style selected rows
-      sx: {
-        fontWeight: row.getIsSelected() ? "bold" : "normal",
-        color: "white",
-      },
-    }),
-
-    muiTableBodyCellProps: ({ column }) => ({
-      //conditionally style pinned columns
-      sx: {
-        fontWeight: column.getIsPinned() ? "bold" : "normal",
-        color: "white",
-        borderBottom: "none",
-        marginBottom: "3px",
-      },
-    }),
-
-    muiTableBodyProps: {
-      sx: (theme) => ({
-        table: {
-          height: "100% !important",
-          borderCollapse: "collapse",
-        },
-        thead: {
-          flex: `0 0 auto`,
-        },
-        tbody: {
-          display: "block",
-          flex: `1 1 auto !important`,
-          overflowY: "auto !important",
-        },
-        "& td": {
-          height: `50px !important`,
-        },
-        "& tr": {
-          padding: "1.5px",
-        },
-        '& tr:nth-of-type(odd):not([data-selected="true"]):not([data-pinned="true"]) > td':
-          {
-            // backgroundColor: darken(baseBackgroundColor, 0.1),
-            backgroundColor: "#1c1c1e",
-          },
-        '& tr:nth-of-type(odd):not([data-selected="true"]):not([data-pinned="true"]):hover > td':
-          {
-            backgroundColor: darken(baseBackgroundColor, 0.2),
-          },
-        '& tr:nth-of-type(even):not([data-selected="true"]):not([data-pinned="true"]) > td':
-          {
-            // backgroundColor: darken(baseBackgroundColor, 0.1),
-            backgroundColor: "#1c1c1e",
-          },
-        '& tr:nth-of-type(even):not([data-selected="true"]):not([data-pinned="true"]):hover > td':
-          {
-            backgroundColor: darken(baseBackgroundColor, 0.2),
-          },
-      }),
-    },
-
-    // muiSelectCheckboxProps: (row, table) => ({
-    //     // checked: (selectedRowIds[row?.row?.original?._id] || selectAcross?.selected) && !selectAcross?.unSelected?.includes(row?.row?.original?._id),
-    //     color: 'primary',
-    //     // onChange: (e) => {
-    //     //     // console.log(table.getSelectedRowModel().rows);
-    //     //     handleRowClick(e, row.row.original._id)
-    //     // },
-    //     // inputProps: {
-    //     //     'aria-label': `select row ${row.row.original.name}`,
-    //     // },
-    //     style: {
-    //       color: '#B3B3BF',
-    //     },
-    //     // size: 'small',
-    // }),
-
-    // Checkbox Design..
-    muiSelectCheckboxProps: {
-      sx: {
-        color: "#0094FF",
-        "& .MuiSvgIcon-root": {
-          color: "#B3B3BF",
-          height: "25px",
-          width: "25px",
-          borderRadius: "5px",
-        },
-        "&.Mui-checked .MuiSvgIcon-root": {
-          color: "#0094FF",
-          backgroundColor: "none",
-        },
-      },
-    },
-
-    // Rows per page area..
-    // Bottom pagination and page numbers area..
-    muiBottomToolbarProps: {
-      className: "mui-table-footer",
-
-      sx: {
-        "& .MuiBox-root": {
-          className: "mui-table-footer",
-          position: "relative",
-          justifyContent: "center",
-        },
-
-        "& .MuiTablePagination-root": {
-          color: "#ffffff",
-          border: "none",
-          boxShadow: "none",
-          display: "flex",
-          flexDirection: "row-reverse",
-
-          // The Pagination bars wrapper..
-          // '& .MuiPagination-root': {
-          //   marginRight: '10px',
-          // }
-
-          // '&.Mui-selected': {
-          //   backgroundColor: '#1c1c1e',
-          //   color: '#ffffff',
-          // },
-          // '&.Mui-selected:hover': {
-          //   backgroundColor: '#1c1c1e',
-          //   color: '#ffffff',
-          // },
-          // '&.Mui-disabled': {
-          //   backgroundColor: '#1c1c1e',
-          //   color: '#ffffff',
-          // },
-          // '&.Mui-disabled:hover': {
-          //   backgroundColor: '#1c1c1e',
-          //   color: '#ffffff',
-          // },
-        },
-
-        // Row Page Numbers Section..
-        "& .MuiInputBase-root": {
-          className: "mui-footer-rows-per-page-input",
-          // right: '-25rem',
-          backgroundColor: "#1C1C1E",
-          padding: "0 10px",
-          borderRadius: "5px",
-        },
-
-        // Text Style..
-        "& .MuiFormLabel-root": {
-          // color: '#ffffff',
-          display: "none",
-        },
-        // Input Style..
-        "& .MuiInput-root": {
-          color: "#ffffff",
-        },
-        // Icon Style..
-        "& .MuiSelect-iconStandard": {
-          color: "#ffffff",
-        },
-      },
-    },
-
-    // Pagination Specific Area..
-    muiPaginationProps: {
-      rowsPerPageOptions: [15, 30, 60, 90, 180],
-      sx: {
-        color: "primary",
-        shape: "circular",
-        variant: "outlined",
-
-        "& .MuiPaginationItem-root": {
-          color: "#8998B8", // Change this to your desired color for pagination numbers
-        },
-        "& .MuiPaginationItem-page.Mui-selected": {
-          backgroundColor: "#1976d2", // Change this to your desired color for selected page numbers
-          color: "#ffffff !important",
-        },
-        "& .MuiTablePagination-root": {
-          color: "#8998B8", // Change this to your desired color for "Rows per page" text
-        },
-        "& .MuiSelect-select": {
-          color: "#8998B8", // Change this to your desired color for dropdown numbers
-        },
-        "& .MuiMenuItem-root": {
-          color: "#8998B8", // Change this to your desired color for dropdown menu items
-        },
-        "& .MuiInputBase-root": {
-          color: "#fffffff !important",
-        },
-      },
-    },
-
-    mrtTheme: (theme) => ({
-      baseBackgroundColor: baseBackgroundColor,
-      draggingBorderColor: theme.palette.secondary.main,
-    }),
-  });
 
   //note: you can also pass table options as props directly to <MaterialReactTable /> instead of using useMaterialReactTable
   //but the useMaterialReactTable hook will be the most recommended way to define table options
@@ -561,17 +274,3 @@ export default function Listing2(props) {
   );
 }
 
-// const url = new URL(
-//   '/api/data',
-//   process.env.NODE_ENV === 'production'
-//     ? 'https://www.material-react-table.com'
-//     : 'http://localhost:3000',
-// );
-// url.searchParams.set(
-//   'start',
-//   `${pagination.pageIndex * pagination.pageSize}`,
-// );
-// url.searchParams.set('size', `${pagination.pageSize}`);
-// url.searchParams.set('filters', JSON.stringify(columnFilters ?? []));
-// url.searchParams.set('globalFilter', globalFilter ?? '');
-// url.searchParams.set('sorting', JSON.stringify(sorting ?? []));
---
+++ src/components/common/SSListing/style/Style.js
@@ -0,0 +1,293 @@
+import { darken } from "@mui/material";
+
+
+export const MuiListStyleProps = (theme) => {
+    const baseBackgroundColor =
+    theme.palette.mode === "dark" ? "rgb(40, 44, 52)" : "#000000";
+    
+    return {
+          //pagination setting end
+    //styling props
+    muiTableContainerProps: { className: "mui-table-container" },
+    muiTablePaperProps: {
+      elevation: 0,
+      sx: {
+        borderRadius: "5px",
+      },
+    },
+
+    // muiSelectCheckboxProps:(row, table) => ({
+    //     checked: (selectedRowIds[row?.row?.original?._id] || selectAcross?.selected) && !selectAcross?.unSelected?.includes(row?.row?.original?._id),
+    //     // color: 'primary',
+    //     onChange: (e) => {
+    //         // console.log(table.getSelectedRowModel().rows);
+    //         handleRowClick(e, row.row.original._id)
+    //     },
+    //     inputProps: {
+    //         'aria-label': `select row ${row.row.original.name}`,
+    //     },
+    //     // style: {
+    //     //   color: 'green',
+    //     // },
+    //     // size: 'small',
+    // }),
+
+    muiTableHeadProps: {
+      // Customizing table head cells
+      sx: {
+        "& .MuiTableCell-root": {
+          borderBottom: "none",
+        },
+        "& .MuiTableSortLabel-root": {
+          color: "#ffffff", // Change color of sort label text
+        },
+      },
+    },
+
+    muiTableHeadCellProps: {
+      //simple styling with the `sx` prop, works just like a style prop in this example
+      sx: {
+        fontWeight: "normal",
+        fontSize: "14px",
+        color: "#ffffff",
+        backgroundColor: "#000000",
+        height: "50px !important",
+
+        // Table Heading's Icons Color..
+        "& .MuiTableSortLabel-icon": {
+          color: "#ffffff !important", // Sort icon color
+        },
+
+        // Table Heading's Icons Color..
+        // Checkbox Icon on Header..
+        "& .MuiSvgIcon-root": {
+          color: "#B3B3BF",
+          height: "25px",
+          width: "25px",
+          borderRadius: "5px",
+        },
+        // Checked Situation on header checkbox
+        "& .Mui-checked .MuiSvgIcon-root": {
+          color: "#1976d2",
+          backgroundColor: "none",
+        },
+        // Interminate Situation on header checkbox
+        "& .MuiCheckbox-indeterminate .MuiSvgIcon-root": {
+          color: "#1976d2",
+          backgroundColor: "none",
+        },
+
+        "& .MuiCheckbox-root": {
+          color: "#0094FF",
+          backgroundColor: "none",
+        },
+
+        // Other Icons on Header..
+        "& .MuiSvgIcon-root:first-child": {
+          // marginRight: '10px',
+          // marginLeft: '10px',
+          color: "#ffffff",
+        },
+        "& span": {
+          color: "#ffffff",
+        },
+      },
+    },
+
+    // Body Styling
+    muiTableBodyRowProps: ({ row }) => ({
+      //conditionally style selected rows
+      sx: {
+        fontWeight: row.getIsSelected() ? "bold" : "normal",
+        color: "white",
+      },
+    }),
+
+    muiTableBodyCellProps: ({ column }) => ({
+      //conditionally style pinned columns
+      sx: {
+        fontWeight: column.getIsPinned() ? "bold" : "normal",
+        color: "white",
+        borderBottom: "none",
+        marginBottom: "3px",
+      },
+    }),
+
+    muiTableBodyProps: {
+      sx: (theme) => ({
+        table: {
+          height: "100% !important",
+          borderCollapse: "collapse",
+        },
+        thead: {
+          flex: `0 0 auto`,
+        },
+        tbody: {
+          display: "block",
+          flex: `1 1 auto !important`,
+          overflowY: "auto !important",
+        },
+        "& td": {
+          height: `50px !important`,
+        },
+        "& tr": {
+          padding: "1.5px",
+        },
+        '& tr:nth-of-type(odd):not([data-selected="true"]):not([data-pinned="true"]) > td':
+          {
+            // backgroundColor: darken(baseBackgroundColor, 0.1),
+            backgroundColor: "#1c1c1e",
+          },
+        '& tr:nth-of-type(odd):not([data-selected="true"]):not([data-pinned="true"]):hover > td':
+          {
+            backgroundColor: darken(baseBackgroundColor, 0.2),
+          },
+        '& tr:nth-of-type(even):not([data-selected="true"]):not([data-pinned="true"]) > td':
+          {
+            // backgroundColor: darken(baseBackgroundColor, 0.1),
+            backgroundColor: "#1c1c1e",
+          },
+        '& tr:nth-of-type(even):not([data-selected="true"]):not([data-pinned="true"]):hover > td':
+          {
+            backgroundColor: darken(baseBackgroundColor, 0.2),
+          },
+      }),
+    },
+
+    // muiSelectCheckboxProps: (row, table) => ({
+    //     // checked: (selectedRowIds[row?.row?.original?._id] || selectAcross?.selected) && !selectAcross?.unSelected?.includes(row?.row?.original?._id),
+    //     color: 'primary',
+    //     // onChange: (e) => {
+    //     //     // console.log(table.getSelectedRowModel().rows);
+    //     //     handleRowClick(e, row.row.original._id)
+    //     // },
+    //     // inputProps: {
+    //     //     'aria-label': `select row ${row.row.original.name}`,
+    //     // },
+    //     style: {
+    //       color: '#B3B3BF',
+    //     },
+    //     // size: 'small',
+    // }),
+
+    // Checkbox Design..
+    muiSelectCheckboxProps: {
+      sx: {
+        color: "#0094FF",
+        "& .MuiSvgIcon-root": {
+          color: "#B3B3BF",
+          height: "25px",
+          width: "25px",
+          borderRadius: "5px",
+        },
+        "&.Mui-checked .MuiSvgIcon-root": {
+          color: "#0094FF",
+          backgroundColor: "none",
+        },
+      },
+    },
+
+    // Rows per page area..
+    // Bottom pagination and page numbers area..
+    muiBottomToolbarProps: {
+      className: "mui-table-footer",
+
+      sx: {
+        "& .MuiBox-root": {
+          className: "mui-table-footer",
+          position: "relative",
+          justifyContent: "center",
+        },
+
+        "& .MuiTablePagination-root": {
+          color: "#ffffff",
+          border: "none",
+          boxShadow: "none",
+          display: "flex",
+          flexDirection: "row-reverse",
+
+          // The Pagination bars wrapper..
+          // '& .MuiPagination-root': {
+          //   marginRight: '10px',
+          // }
+
+          // '&.Mui-selected': {
+          //   backgroundColor: '#1c1c1e',
+          //   color: '#ffffff',
+          // },
+          // '&.Mui-selected:hover': {
+          //   backgroundColor: '#1c1c1e',
+          //   color: '#ffffff',
+          // },
+          // '&.Mui-disabled': {
+          //   backgroundColor: '#1c1c1e',
+          //   color: '#ffffff',
+          // },
+          // '&.Mui-disabled:hover': {
+          //   backgroundColor: '#1c1c1e',
+          //   color: '#ffffff',
+          // },
+        },
+
+        // Row Page Numbers Section..
+        "& .MuiInputBase-root": {
+          className: "mui-footer-rows-per-page-input",
+          // right: '-25rem',
+          backgroundColor: "#1C1C1E",
+          padding: "0 10px",
+          borderRadius: "5px",
+        },
+
+        // Text Style..
+        "& .MuiFormLabel-root": {
+          // color: '#ffffff',
+          display: "none",
+        },
+        // Input Style..
+        "& .MuiInput-root": {
+          color: "#ffffff",
+        },
+        // Icon Style..
+        "& .MuiSelect-iconStandard": {
+          color: "#ffffff",
+        },
+      },
+    },
+
+    // Pagination Specific Area..
+    muiPaginationProps: {
+      rowsPerPageOptions: [15, 30, 60, 90, 180],
+      sx: {
+        color: "primary",
+        shape: "circular",
+        variant: "outlined",
+
+        "& .MuiPaginationItem-root": {
+          color: "#8998B8", // Change this to your desired color for pagination numbers
+        },
+        "& .MuiPaginationItem-page.Mui-selected": {
+          backgroundColor: "#1976d2", // Change this to your desired color for selected page numbers
+          color: "#ffffff !important",
+        },
+        "& .MuiTablePagination-root": {
+          color: "#8998B8", // Change this to your desired color for "Rows per page" text
+        },
+        "& .MuiSelect-select": {
+          color: "#8998B8", // Change this to your desired color for dropdown numbers
+        },
+        "& .MuiMenuItem-root": {
+          color: "#8998B8", // Change this to your desired color for dropdown menu items
+        },
+        "& .MuiInputBase-root": {
+          color: "#fffffff !important",
+        },
+      },
+    },
+
+    mrtTheme: (theme) => ({
+      baseBackgroundColor: baseBackgroundColor,
+      draggingBorderColor: theme.palette.secondary.main,
+    }),
+    }
+
+}
